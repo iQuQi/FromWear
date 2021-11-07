c5493b5f5173063afabd7e1773763341c82a6489
= <kidscop99@naver.com>
2021-11-07 16:37:40 +0900

    search page

---
+++ fromwear/src/SearchPage/SearchPage.js
@@ -0,0 +1,24 @@
+
+import * as React from 'react';
+import {Component} from 'react';
+import './SearchPage.css'
+
+
+
+class SearchPage extends Component{
+	constructor(){
+		super();
+	}
+
+	render(){
+		return(
+			<div className="container">		
+
+			</div>
+		)
+	}
+
+
+}
+
+export default Header;
