423d6d1ce44d1a03d58a731f93bfb5866c893378
zzzl-523 <zzh23@naver.com>
2021-11-14 15:24:40 +0900

    add url path

--- fromwear/src/App.js
+++ fromwear/src/App.js
@@ -3,14 +3,24 @@ import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import MainPage from './MainPage/MainPage.jsx';
+import TodayPostBoard from './PostBoard/TodayPostBoard.jsx'
+import PostView from './PostView/Post.jsx'
+import PostWritePage from './PostWritePage/PostWritePage.jsx'
+import SearchPage from './SearchPage/SearchPage.jsx'
+import WeeklyTagPage from './WeeklyTagPage/WeeklyTagPage.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-      	<MainPage />
-			</header>
-    </div>
+    <div className='App'>
+        <BrowserRouter>
+          <Route path="/" component={MainPage} exact/>
+          <Route path="/todayboard" component={TodayPostBoard} exact/>
+          <Route path="/post" component={PostView} exact/>
+          <Route path="/posting" component={PostWritePage} exact/>
+          <Route path="/search" component={SearchPage} exact/>
+          <Route path="/weeklytag" component={WeeklyTagPage} exact/>
+        </BrowserRouter>
+      </div>
   );
 }
 
