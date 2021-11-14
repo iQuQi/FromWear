6738e3f1ac1bf8c4b98c587468285982da3127c1
cjm0611 <cjm-0611@naver.com>
2021-11-14 10:52:37 +0900

    best_pick

--- fromwear/src/PostView/SingleComment.js
+++ fromwear/src/PostView/SingleComment.js
@@ -3,6 +3,7 @@ import './SingleComment.css';
 import writer_img from './Imgs/pro1.jpeg';
 import Thumb from './Thumb';
 import Select_button from './Select_button';
+import StarIcon from '@mui/icons-material/Star';
 
 let post_id = 1
 
@@ -10,7 +11,10 @@ export default function SingleComment({tweet}){
     return (
         <div>
             <div className="one_comment">
-                <img src={writer_img} className="writer_img" />
+                <div className="star_icon">
+                    <StarIcon />
+                </div>
+                <img src={writer_img} className="writer_img best_pick" />
                 <div className="comment_user_name">{tweet.name}</div>
                 <Thumb />
                 {
