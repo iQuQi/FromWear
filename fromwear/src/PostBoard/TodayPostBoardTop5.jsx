cb5015b42ded349a115094da2c21b887dacaac96
ronci <jyj9647@naver.com>
2021-11-21 03:24:55 +0900

    Split todayPostBoardPosts, Connect Amplify to the TodayPostBoardTop5

--- fromwear/src/PostBoard/TodayPostBoardTop5.jsx
+++ fromwear/src/PostBoard/TodayPostBoardTop5.jsx
@@ -12,40 +12,31 @@ export default class TodayPostBoardTop5 extends Component {
     constructor() {
         super();
         this.state = {
-            post_top_list:[],
-            post_top: ["https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-                "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
-                "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
-                "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
-                "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+            post_top_list : [
+                {
+                    img: "",
+                    like_user_num: "",
+                    user : {
+                        id: "",
+                        profile_img: "",
+                    },
+                },
             ],
-            user_name: ["name1", "name2", "name3", "name4", "name5"],
-            user_profile_img: ["https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-            "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
-            "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
-            "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
-            "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
-            ],
-            like_user_num: ["500", "400", "300", "200", "100"],
         };
     }
 
-    componentDidMount() {
+    componentWillMount() {
 		API.graphql({ 
             query: listPosts, 
-            variables: { filter: {board_type: {eq: 0}} }})
+            variables: { filter: {board_type: {eq: 0}}}})
 			.then(res => this.setState({
-                post_top_list: res.data.listPosts.items.sort(function(a,b){return b.like_user_num-a.like_user_num})
+                post_top_list: res.data.listPosts.items.sort(function(a,b){return b.like_user_num-a.like_user_num}).slice(0, 5)
             }))
 			.catch(e => console.log(e));
 	}
 
-
     render() {
         let {post_top_list} = this.state;
-        let {post_top, user_name, user_profile_img, like_user_num} = this.state;
-        console.log(post_top_list[0]);
-        
 
 		const settings = {
 			className: "center",
@@ -64,37 +55,37 @@ export default class TodayPostBoardTop5 extends Component {
                         <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
                         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
                         <Slider {...settings}>
-                            {post_top.map((url, index) => (
+                            {post_top_list.map((post, index) => (
                                     <div className="div_test">
-                                        <img style={{backgroundImage: `URL(${url})`}}></img>
+                                        <img style={{backgroundImage: `URL(${post.img})`}}></img>
                                         <a href="/post">
                                             <span className={"dimmed_layer"}>
                                                 <span className="dimmed_info_writer">
-                                                    <img src={user_profile_img[index]} alt="프로필" 
+                                                    <img src={post.user.profile_img} alt="프로필" 
                                                         style={{width:"30px",height:"30px",borderRadius:"50%", 
-                                                        position: "relative",top:"10px", left:"5px", marginLeft:"5px"}}/>
-                                                    {user_name[index]}
+                                                        position: "relative",top:"10px", left:"3px", marginLeft:"5px"}}/>
+                                                    {post.user.id}
                                                 </span>
                                                 <span className="dimmed_info_like">
-                                                    {like_user_num[index]}<FavoriteIcon style={{fontSize: 20,position:"relative",top:5, marginLeft:5}}/>
+                                                    {post.like_user_num}<FavoriteIcon style={{fontSize: 20,position:"relative",top:5, marginLeft:5}}/>
                                                 </span>
                                             </span>
 									    </a>
                                     </div>
                             ))}
-                            {post_top.map((url, index) => (
+                            {post_top_list.map((post, index) => (
                                     <div className="div_test">
-                                        <img style={{backgroundImage: `URL(${url})`}}></img>
+                                        <img style={{backgroundImage: `URL(${post.img})`}}></img>
                                         <a href="/post">
                                             <span className={"dimmed_layer"}>
                                                 <span className="dimmed_info_writer">
-                                                    <img src={user_profile_img[index]} alt="프로필" 
+                                                    <img src={post.user.profile_img} alt="프로필" 
                                                         style={{width:"30px",height:"30px",borderRadius:"50%", 
-                                                        position: "relative",top:"10px", left:"5px", marginLeft:"5px"}}/>
-                                                    {user_name[index]}
+                                                        position: "relative",top:"10px", left:"3px", marginLeft:"5px"}}/>
+                                                    {post.user.id}
                                                 </span>
                                                 <span className="dimmed_info_like">
-                                                    {like_user_num[index]}<FavoriteIcon style={{fontSize: 20,position:"relative",top:5, marginLeft:5}}/>
+                                                    {post.like_user_num}<FavoriteIcon style={{fontSize: 20,position:"relative",top:5, marginLeft:5}}/>
                                                 </span>
                                             </span>
 									    </a>
