3256a3638d2e4c8c8105608a87e55be59d2f0933
Jihyun Lee <zzh23@naver.com>
2022-05-03 18:39:54 +0900

    로그인 후 이용해주세요

--- fromwear/src/FeedPage/FeedPageM.jsx
+++ fromwear/src/FeedPage/FeedPageM.jsx
@@ -9,24 +9,33 @@ export default class FeedPageM extends Component {
         super();
 
 		this.state = {
-			now_user:{},
+			now_user: 'noUser',
 		};
 	}
     
 
     handle_user_info = (user) => {
 		
-		this.setState({now_user: user,});
+		this.setState({
+            now_user: user,
+        });
 		
 	}
     
     
     render(){
-        let {now_user} = this.state;
+        let {now_user, after} = this.state;
         
         return(
             <div className='mobile_wrap'>
                 <Header handle_user_info={this.handle_user_info}/>
+                {
+                    this.state.now_user=='noUser'?
+                    <p style={{marginTop: '100px'}}>
+                        로그인 후 이용해주세요.
+                    </p>
+                    :null
+                }
                 <FeedPost user={now_user} />
                 <BottomTab />
             </div>
