76163952e6e3845cbfde5039fac810a48e30060c
Jihyun Lee <zzh23@naver.com>
2022-05-05 23:12:27 +0900

    탭 변경중

--- fromwear/src/MyPage/MyPageBodyM.jsx
+++ fromwear/src/MyPage/MyPageBodyM.jsx
@@ -5,6 +5,9 @@ import MyPageButtonGroup from './MyPageButtonGroup';
 import ProfileEdit from '../ProfileEditPage/ProfileEdit';
 import BottomTab from '../BottomNavigation/BottomNavigation';
 
+import MyPostBoard from './MyPostBoard';
+import MyPostBoardSOS from './MyPostBoard_SOS';
+
 export default function MyPageBodyM({now_user}) {
     const [is_profile_edit, set_is_profile_edit] = React.useState(false);
 
@@ -29,9 +32,21 @@ export default function MyPageBodyM({now_user}) {
                     <div className='mypage_contents'>
                         <ProfileM user={now_user} handle_profile_edit={handle_profile_edit}/>
                         
-                        <div id = 'tab' className = 'mypage_collection'>
-                            <MyPageButtonGroup user={now_user}/>
-                        </div>		
+                        <div>
+                            <ul>
+                                <li>
+                                    <button onClick={<MyPostBoard user={now_user}/>}>
+                                        게시판
+                                    </button>
+                                </li>
+                                <li>
+                                    <button onClick={<MyPostBoardSOS user={now_user}/>}>
+                                        SOS
+                                    </button>
+                                </li>
+                            </ul>
+                        </div>
+                        	
                     </div>
                 </div>
             }
